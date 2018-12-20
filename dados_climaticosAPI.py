b738dd93c2d4b8143f8f480dbb3a60b2d6933f03
andressamesquita <33494285+andressamesquita@users.noreply.github.com>
2018-12-20 15:21:24 -0300

    Update dados_climaticosAPI.py

--- dados_climaticosAPI.py
+++ dados_climaticosAPI.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
-city = input('Cidade: ')     #'London'
-key = input('Chave: ')       #'c20a21ddb76e3c3d5fe17c18c7dfb852'
+city = input('Cidade: ')     
+key = input('Chave: ')      
 url =('http://api.openweathermap.org/data/2.5/weather?q=%s,uk&APPID=%s' % (city, key))
 response = requests.get(url).json()
 
