5c777167268b2365626f8067be3dbad6b5960910
andressamesquita <33494285+andressamesquita@users.noreply.github.com>
2018-12-20 15:23:03 -0300

    Update DeezerApi.py

--- DeezerApi.py
+++ DeezerApi.py
@@ -6,9 +6,9 @@ url = "https://api.deezer.com/user/2490398346/playlists"
 response = requests.get(url).json()
 
 #playlist
-#print(response)
 print(" Titulo: %s \n duration: %s" %(response['data'][0]['title'], response['data'][0]['duration']))
+
+#print(response)
 
 #album
 #print(" Titulo: %s \n Autor: %s \n Data de lançamento: %s \n Número de músicas: %s" %(response['title'], response['artist']['name'], response['release_date'], response['nb_tracks'] ))
-
