b7254fa63e2bc46d21e24266f083973ae4f38cd8
andressamesquita <33494285+andressamesquita@users.noreply.github.com>
2018-12-20 15:23:42 -0300

    Update sentimentAPI.py

--- sentimentAPI.py
+++ sentimentAPI.py
@@ -5,7 +5,7 @@ url = 'https://community-sentiment.p.mashape.com/text/'
 
 response = unirest.post(url,
   headers={
-    "X-Mashape-Key": "2MnpUHUHWxmshXaPTMszTVs5NUGep1ohd7Jjsnw8ECU77MCTTU",
+    "X-Mashape-Key": "suachave",
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "application/json"
   },
